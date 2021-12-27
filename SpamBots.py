40cd85fa81de83df30f2e12fffb49bc48f72eb67
ALCOHOLIC <87254338+Usermax77@users.noreply.github.com>
2021-12-27 06:59:23 +0530

    Update SpamBots.py
    Chnage

--- SpamBots.py
+++ SpamBots.py
@@ -724,7 +724,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None)
         end = datetime.now()
         ms = (end - start).microseconds / 1000
-        await event.edit(f"👅PONG!\n`{ms}` NET\n ▀█ █▀█ █▄░█ █▀▀
+        await event.edit(f"👅PONG!\n`{ms}` ms\n ▀█ █▀█ █▄░█ █▀▀
 █▀▀ █▄█ █░▀█ █▄█
 ")
 
